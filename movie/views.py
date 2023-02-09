1d507f095e377cbd227030314e5b27750104316d
juan-c-s <juancamilo.salazar2002@gmail.com>
2023-02-09 11:10:54 -0500

    Change Color

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,"home.html",{'name': 'Juan Camilo'})
+
+def about(request):
+    return render(request,"about.html")
