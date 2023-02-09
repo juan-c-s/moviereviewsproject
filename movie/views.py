984c8a0095105129353658bd6276019876d61b0b
juan-c-s <juancamilo.salazar2002@gmail.com>
2023-02-09 11:19:42 -0500

    flex

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,"home.html",{'name': 'Juan Camilo'})
+    return render(request,"home.html",{'name': ' Juan Camilo'})
 
 def about(request):
     return render(request,"about.html")
